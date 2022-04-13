35bfc42a9ccb16458b8686908220c9af0e23d6bd
lxg13 <1096807215@qq.com>
2022-04-13 13:55:54 +0800

    Initial commit

---
+++ extension/src/register_types.cpp
@@ -0,0 +1,53 @@
+/*
+# MIT License
+
+Copyright (c) 2017-2022 Godot Engine contributors.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include "register_types.h"
+#include "summator.h"
+#include <godot/gdnative_interface.h>
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+void register_summator_types()
+{
+	godot::ClassDB::register_class<Summator>();
+}
+
+void unregister_summator_types() {}
+
+extern "C"
+{
+
+	// Initialization.
+
+	GDNativeBool GDN_EXPORT summator_library_init(const GDNativeInterface *p_interface, const GDNativeExtensionClassLibraryPtr p_library, GDNativeInitialization *r_initialization)
+	{
+		godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+
+		init_obj.register_scene_initializer(register_summator_types);
+		init_obj.register_scene_terminator(unregister_summator_types);
+
+		return init_obj.init();
+	}
+}
